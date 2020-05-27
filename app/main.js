7f2f2a1064d8759126d74cedac89df2fe12c8429
Vickey <karnan.796@gmail.com>
2020-05-27 17:56:59 +0530

    initial commit

--- app/main.js
+++ app/main.js
@@ -1,1 +1,36 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ show: false });
+    mainWindow.loadFile(`${__dirname}/index.html`);
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+});
+
+exports.getFileFromUser = () => {
+    //return undefined if user clicks on the cancel or array if user choose a file
+    const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            {
+                name: 'Markdown Files', // you can name whatever you want which will be displayed in the dropdown options in mac 
+                extensions: ['md', 'mdown'] // files with this extensions will be enabled for to pick
+            }, {
+                name: 'Text Files', extensions: ['txt', 'text']
+            }
+        ]
+    });
+    if (!files) return;
+
+    const file = files[0];
+    openFile(file);
+};
+
+const openFile = (file) => {
+    const content = fs.readFileSync(file).toString();
+    mainWindow.webContents.send('file-opened', file, content); // from main process we are sending the message to the render process
+};
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,6 @@
 const marked = require('marked');
+const { remote, ipcRenderer } = require('electron');
+const mainProcess = remote.require('./main');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -17,4 +19,13 @@ const renderMarkdownToHtml = markdown => {
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
+});
+
+openFileButton.addEventListener('click', () => {
+  mainProcess.getFileFromUser();
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content)
 });
